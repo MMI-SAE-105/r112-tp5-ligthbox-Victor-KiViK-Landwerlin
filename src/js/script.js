7ef51d39178ab8c0aa558f33642497fdd7040853
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2024-12-04 15:00:19 +0100

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,17 @@
 // *** LIGHTBOX ***
 const lb = document.querySelector("#lightbox");
+const images = document.querySelectorAll("[data-full-img]");
 
-lb.showModal()
+
+images.forEach(image => {
+    image.addEventListener("click", function() {
+        const fullImg = image.dataset.fullImg;
+        const i = lb.querySelector("img");
+        i.src = fullImg;
+        lb.showModal();
+    });
+});
 
 lb.addEventListener("click", function() {
     lb.close();
-    });
+});
